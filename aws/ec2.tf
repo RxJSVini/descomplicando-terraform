5f3d7006290f4d474196404f90f79946c482c272
RxJSVini <viniciuspoa2@hotmail.com>
2023-09-01 22:29:36 -0500

    enviando curso na branch develop

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -9,10 +9,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Ubuntu
 }
 
+
+data "aws_ami" "ubuntu-west-2" {
+  provider = aws.west-2
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
 resource "aws_instance" "web" {
 
   provider = aws.west-2
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu-west-2.id
   instance_type = "t2.micro"
 
   tags = {
